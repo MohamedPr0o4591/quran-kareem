482348201886df5d8fdac3b46cb8ff662a94a680
mohamedpr0o4591 <ev9ry.24hours@gmail.com>
2024-09-22 22:23:34 +0300

    updates

--- src/pages/all quran/AllQuranPage.jsx
+++ src/pages/all quran/AllQuranPage.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllQuran } from "../../Redux/Actions/actions";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Pagination, Stack } from "@mui/material";
 import "./AllQuranPage.css";
 import FinalMessage from "../../components/FinalMessage";
 
@@ -9,14 +9,36 @@ function AllQuranPage() {
   const data = useSelector((state) => state.GET_SURAHS.surahs);
   const dispatch = useDispatch();
 
+  const [filterPages, setFilterPages] = useState([]);
+  const [pageCount, setPageCount] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
   useEffect(() => {
     document.title = "المصحف كاملاً";
     dispatch(getAllQuran());
   }, []);
+
+  useEffect(() => {
+    const newData = data
+      .map((surah) => ({
+        ...surah,
+        ayahs: surah.ayahs.filter((ayah) => ayah.page === currentPage),
+      }))
+      .filter((surah) => surah.ayahs.length > 0);
+
+    const pageCount = [
+      ...new Set(data.flatMap((surah) => surah.ayahs.map((ayah) => ayah.page))),
+    ];
+
+    setPageCount(pageCount);
+
+    setFilterPages(newData);
+  }, [data, currentPage]);
+
   return (
     <div className="allQuranPage py-3">
       <Container>
-        {data?.map((surah, index) => {
+        {filterPages?.map((surah, index) => {
           return (
             <Box key={index}>
               {surah.name !== data[index - 1]?.name ? (
@@ -43,10 +65,20 @@ function AllQuranPage() {
             </Box>
           );
         })}
-
-      <FinalMessage />
-
       </Container>
+
+      <div>
+        <Stack spacing={2} alignItems={"center"}>
+          <Pagination
+            count={pageCount.length}
+            shape="rounded"
+            sx={{ direction: "ltr" }}
+            onChange={(e, page) => setCurrentPage(page)}
+          />
+        </Stack>
+
+        <FinalMessage />
+      </div>
     </div>
   );
 }
